7c791535c60b98d2c0913dbdfb7d6bfc25c31342
eriksan21 <76129764+eriksan21@users.noreply.github.com>
2020-12-17 13:16:33 +0600

    Update Pictograms.js

--- src/sections/navigation/Pictograms.js
+++ src/sections/navigation/Pictograms.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Pictograms = () => {
   return (
     <div className='center'>
-      <Image src='/img115.png' width={1000} height={600}/>
+      <Image src='/img115.png' width={1024} height={681}/>
     </div>
   );
 };
